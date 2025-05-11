880e580b8854f14a6ffe349c56dafda22d22368f
steve <za19012397@zapopan.tecmm.edu.mx>
2025-05-10 21:02:25 -0600

    feat: Se agrego mas condigo para mostrar informacion sobre el tipo de kernel

--- signatureInfo.go
+++ signatureInfo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"runtime"
 	"os"
+	"os/exec"
 )
 
 func main(){
@@ -11,8 +12,22 @@ func main(){
 	fmt.Println("Arquitectura:",runtime.GOARCH)
 
 	hostname, _ := os.Hostname()
+
+	cmd1 := exec.Command("uname","-a")
+	kernelInfo, err := cmd1.CombinedOutput()
+	if err != nil {
+		kernelInfo = []byte("Desconocido")
+	}
+
+	cmd2 := exec.Command("cat","/proc/version")
+	kernelInfoSecond, err := cmd2.CombinedOutput()
+	if err != nil {
+		kernelInfoSecond =  []byte("Desconocido")
+	}
 	
 	fmt.Println("Host:",hostname)
 	fmt.Println("Usuario:", os.Getenv("USER"))
 	fmt.Println("Shell:", os.Getenv("SHELL"))
+	fmt.Println("Info Kernel 1:",string(kernelInfo))
+	fmt.Println("Info Kernel 2:",string(kernelInfoSecond))
 }
